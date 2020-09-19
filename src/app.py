abc70a3e5300fa75a7eeddce8b8e4fc2bf5fc3fc
sumukshashidhar <sumukuuu@gmail.com>
2020-09-19 13:33:47 +0530

    :bug: Removed Https

--- src/app.py
+++ src/app.py
@@ -49,7 +49,7 @@ def accept_incoming_image():
         file_uuid = shortuuid.uuid()
         filename = storage_directory + file_uuid + ".png"
         f.save(filename)
-        ilog.log(response[1]['username'], file_uuid+".png", f"https://40.76.37.214:80/static/images/{response[1]['username']}/{file_uuid}.png")
+        ilog.log(response[1]['username'], file_uuid+".png", f"40.76.37.214:80/static/images/{response[1]['username']}/{file_uuid}.png")
         return(flask.jsonify({
             "status":200,
             "message":f"Accepted the Image from user {response[1]['username']}"
