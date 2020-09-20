36a235ceb12fb5bd06b6325585ebabdb070996d3
AAA_shuibiao <aaashuibiao@gmail.com>
2020-09-20 04:25:51 -0400

    ✨Added facial detection function

--- src/machine_learning/face_detection/detect.py
+++ src/machine_learning/face_detection/detect.py
@@ -1,4 +1,9 @@
+import cv2 as cv
+import sys
+
+
 def detect(input_filename, output_filename):
+
     """Takes in input and output files and
 
     Args:
@@ -8,4 +13,25 @@ def detect(input_filename, output_filename):
     Returns:
         dict: returns a keyvalue pair with executed? as True
     """
-    return {"executed":True}
+
+
+    original_image = cv.imread(input_filename)
+    grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+
+    detected_faces = {}
+
+
+    i = 0
+    for face in face_cascade.detectMultiScale(grayscale_image):
+        detected_faces["face" + str(i)] = list(face)
+        i += 1
+    
+    detected_faces["executed"] = True
+
+    return detected_faces
+
+
+if __name__ == "__main__":
+    print( detect("faceTest.jpg", "") )
