d5616b586fca4b68a96dc1b0070c2bb0b431b03a
sumukshashidhar <sumukuuu@gmail.com>
2020-09-20 15:17:57 +0530

    :bug: Tensorflow 1 compat

--- src/machine_learning/handwriting_recognition/Model.py
+++ src/machine_learning/handwriting_recognition/Model.py
@@ -3,9 +3,10 @@ from __future__ import print_function
 
 import sys
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import os
 
+tf.disable_v2_behavior()
 
 class DecoderType:
 	BestPath = 0
