0c3c6c702abfa59ecd4d73b650365ce3a595b295
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-09-20 17:09:00 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -236,7 +236,7 @@ def ml_face_recog():
                      
                      
 @app.route('/api/ml/classifier', methods=['GET'])
-def classifier():
+def classification():
     # retrieve the last image from the database
     with open('userlist.csv', 'r') as f:
         # readlines, select the last line and split
