ba4e367fae4e655084a523717d2e84e7398cb675
sumukshashidhar <sumukuuu@gmail.com>
2020-09-20 09:45:37 +0530

    :loud_sound: Updated logging capabilities and userlist.csv file

--- src/image_upload_logger.py
+++ src/image_upload_logger.py
@@ -3,3 +3,4 @@ def log(username, filename, link, time):
     with open("userlist.csv", "a+") as f:
         f.write('\n')
         f.write(f'{time},{username},{filename},{link}')
+        f.write('\n')
